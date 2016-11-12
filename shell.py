948dcb1529456b760fef02b39fb56a5ccf065809
copperstick6 <copperstick6@gmail.com>
2016-11-12 16:33:58 -0600

    POST requests now work

--- shell.py
+++ shell.py
@@ -1,6 +1,6 @@
 #basic shell with data for our project
 #data is manually added until further notice
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 import time
 app = Flask(__name__)
@@ -10,16 +10,23 @@ oppList = [["","","",""]]
 curTime = (time.strftime("%d/%m/%Y"))
 print(curTime)
 
-
-
 @app.route('/addData', methods = ['POST'])
 def addingOpps():
     #removing objects that contain times we don't need or want
+    print(oppList)
     for i in oppList:
         if request.form['date'] > curTime:
             newList = [request.form['opportunity'], request.form['curPerson'], request.form['date'], request.form['description']]
             oppList.append(newList)
+    return render_template('index.html')
+
+print(oppList)
 
 @app.route('/curData', methods = ['GET'])
 def returnOpps():
-    return oppList
+    print(oppList)
+    return str(oppList)
+
+
+if __name__ == "__main__":
+    app.run()
