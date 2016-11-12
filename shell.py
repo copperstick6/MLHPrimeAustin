64f762d6dbccc62c000ee735b16044861f392b03
copperstick6 <copperstick6@gmail.com>
2016-11-12 14:26:02 -0600

    added basic shell

---
+++ shell.py
@@ -0,0 +1,13 @@
+#basic shell with data for our project
+#data is manually added until further notice
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+#oppList is the defined as the current Opportunity List
+oppList = [["",""]]
+
+
+
+
+print(oppList)
