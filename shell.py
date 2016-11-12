be98d2526aff0d5e8295a650d89a600a23a74d52
copperstick6 <copperstick6@gmail.com>
2016-11-12 14:49:44 -0600

    added basic shell for adding opportunities manually

--- shell.py
+++ shell.py
@@ -2,12 +2,20 @@
 #data is manually added until further notice
 from flask import Flask
 from flask import request
+import time
 app = Flask(__name__)
 
 #oppList is the defined as the current Opportunity List
-oppList = [["",""]]
+oppList = [["","","",""]]
+curTime = (time.strftime("%d/%m/%Y"))
+print(curTime)
 
 
 
-
-print(oppList)
+@app.route('/addData', methods = ['POST'])
+def addingOpps():
+    #removing objects that contain times we don't need or want
+    for i in oppList:
+        if request.form['date'] > curTime:
+            newList = [request.form['opportunity'], request.form['curPerson'], request.form['date'], request.form['description']]
+            oppList.append(newList)
