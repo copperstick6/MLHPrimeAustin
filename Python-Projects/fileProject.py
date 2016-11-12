c4794d739ef0180859e1dbb6bdfdff7eed1bd5b2
copperstick6 <copperstick6@gmail.com>
2016-11-12 11:15:15 -0600

    added all

--- Python-Projects/fileProject.py
+++
@@ -1,7 +0,0 @@
-#open the file if it exists, write if it doesn't
-#w+ represents create
-f = open("newFile.txt", "w+")
-for x in range (0,10):
-    f.write("this is line %d\r\n" %(x+1))
-
-f.close
--- Python-Projects/internetDataRetrieval.py
+++
@@ -1,9 +0,0 @@
-#importing the URL lib package for python 2
-import urllib2
-#opening the URL
-webUrl = urllib2.urlopen("https://www.cs.utexas.edu/~naren/")
-#getcode returns the status of the request to the website
-print "result code: " + str(webUrl.getcode())
-#read() reads the contents of the code on the website
-data = webUrl.read()
-print data
